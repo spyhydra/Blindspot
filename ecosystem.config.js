ad6d2d8fbf31f0c5d57cf92403175271c6eb9a53
spyhydra <chetan2112001@gmail.com>
2026-04-18 12:57:55 +0530

    fix the issue

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: "blindspot-app",
-      script: ".next/standalone/server.js",
+      script: "npm",
+      args: "run start",
       instances: 1,
       watch: false,
       max_memory_restart: "1G", // Restart app if memory goes over 1GB
