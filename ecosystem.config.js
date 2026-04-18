cf1c27fdd31f585ff40c6faa97ae008965fb6d05
spyhydra <chetan2112001@gmail.com>
2026-04-18 12:24:43 +0530

    file changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,7 @@ module.exports = {
   apps: [
     {
       name: "blindspot-app",
-      script: "node_modules/next/dist/bin/next",
-      args: "start",
+      script: ".next/standalone/server.js",
       instances: 1,
       watch: false,
       max_memory_restart: "1G", // Restart app if memory goes over 1GB
